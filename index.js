463b29bd74ed6e0e0fa1bfb5458e3e41de274e09
lamywmpaul@gmail.com <lamywmpaul@gmail.com>
2024-04-07 19:04:45 -0400

    OPTIONS added

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(cors({
             callback(new Error('Not allowed by CORS'));
         }
     },
-    methods: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
